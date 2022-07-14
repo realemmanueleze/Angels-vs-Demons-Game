ef9d2756b265d3aa3fce8655b12cb9dabc9f835b
Emmanuel Eze <emmanueleze.official@gmail.com>
2022-07-14 16:34:52 +0100

    feat: added gameplay features & endgame features

--- Character.js
+++ Character.js
@@ -2,27 +2,36 @@ import {getDiceRollArray} from '/utils.js'
 
 function Character(data) {
     Object.assign(this, data)
+    this.diceArray = getDicePlaceholderHtml(this.diceCount)
     
-    this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num){ 
-            return  `<div class="dice">${num}</div>`
-        }).join('')
+    this.getDiceHtml = function () {
+        this.currentDiceScore = getDiceRollArray(this.diceCount);
+        this.diceArray = this.currentDiceScore.map((num) =>
+            `<div class="dice">${num}</div>`).join("")
+    }
+    
+    this.takeDamage = function(attackScoreArray){  
+        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
+        this.health -= totalAttackScore
+        if (this.health <= 0){
+            this.dead = true  
+            this.health = 0
+        } 
     }
 
     this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;      
-        let diceHtml = this.getDiceHtml(diceCount);
-        
-           return `
+        const { elementId, name, avatar, health, diceCount } = this;
+
+        return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
                 <div class="dice-container">
-                    ${diceHtml}
+                    ${this.diceArray}
                 </div>
-            </div>`;
-    }  
+            </div>`
+    }
 }
 
 export default Character
